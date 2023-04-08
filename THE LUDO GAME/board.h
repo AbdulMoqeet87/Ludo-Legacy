43fe5ca175b6abd9dd01f186bfe369b7170ad665
AbdulMoqeet87 <abdulmoqeet87@gmail.com>
2023-04-08 11:27:28 +0500

    kata

--- THE LUDO GAME/board.h
+++ THE LUDO GAME/board.h
@@ -11,8 +11,8 @@ public:
 	void drawBoard(WindowRender& window);
 	bool isValidMove(int sri, int sci, int dri, int dci);
 	void Move(int sri, int sci, int dri, int dci);
-	void drawHome();
-
+	//drawHome();
+	
 
 };
 
