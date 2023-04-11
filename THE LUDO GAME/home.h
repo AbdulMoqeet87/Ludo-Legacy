c5fc81af5561d3c09fef922bdeded51d58bef0b0
AmnaAmir6 <amnaamir605@gmail.com>
2023-04-12 03:06:55 +0500

    home cpp

--- THE LUDO GAME/home.h
+++ THE LUDO GAME/home.h
@@ -6,20 +6,20 @@ using namespace sf;
 using namespace std;
 
 
-class Cell;
-
-class home
-{
-private:
-	int InitialPos;
-	int SafeSpot;
-	Cell** Cs;
-	Color C;
-	Sprite Pic;
-public:
-	home(istream& rdr, int IP,int SP, Color _C,string FileName);
-	void Draw(RenderWindow& window);
-	int getSafeSpot();
-	int getInitialPos();
-};
+//class Cell;
+//
+//class home
+//{
+//private:
+//	int InitialPos;
+//	int SafeSpot;
+//	Cell** Cs;
+//	Color C;
+//	Sprite Pic;
+//public:
+//	home(istream& rdr, int IP,int SP, Color _C,string FileName);
+//	void Draw(RenderWindow& window);
+//	int getSafeSpot();
+//	int getInitialPos();
+//};
 
