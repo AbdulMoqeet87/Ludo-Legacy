2cf8153c94c32b4fb8584f3014bc21b0006d370a
AbdulMoqeet87 <abdulmoqeet87@gmail.com>
2023-04-12 00:39:24 +0500

    stageds

--- THE LUDO GAME/cell.cpp
+++ THE LUDO GAME/cell.cpp
@@ -1,1 +1,37 @@
+#include<SFML/Graphics.hpp>
+#include<fstream>
+using namespace std;
+using namespace sf;
 #include "cell.h"
+
+Cell::Cell(int _ri, int _ci, int _s, Color _C)
+{
+    this->ri = _ri;
+    this->ci = _ci;
+    this->size = _s;
+    this->C = _C;
+    Rec.setSize(sf::Vector2f(size, size));
+    Rec.setFillColor(C);
+    Rec.setPosition(ci, ri);
+}
+
+Cell::Cell(istream& rdr)
+{
+    rdr >> ci;
+    rdr >> ri;
+}
+
+void Cell::Draw(RenderWindow& window)
+{
+    window.draw(Rec);
+}
+
+void Cell::Highlight(RenderWindow& window,Color _C)
+{
+    Rec.setFillColor(sf::Color::Cyan);
+}
+
+void Cell::UnHighlight(RenderWindow& window)
+{
+    Rec.setFillColor(C);
+}
--- THE LUDO GAME/cell.h
+++ THE LUDO GAME/cell.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<SFML/Graphics.hpp>
+#include<fstream>
+using namespace std;
 using namespace sf;
 
 class Cell
