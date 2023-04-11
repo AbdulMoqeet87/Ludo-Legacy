e71c039a0a79b9d0941d7b21ba3ba4d3bb3f8f13
AmnaAmir6 <amnaamir605@gmail.com>
2023-04-12 00:18:31 +0500

    Cell class

--- THE LUDO GAME/cell.cpp
+++ THE LUDO GAME/cell.cpp
@@ -1,1 +1,29 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
 #include "cell.h"
+
+Cell::Cell(int _ri, int _ci, int _s, Color _C)
+{
+    this->ri = _ri;
+    this->ci = _ci;
+    this->size = _s;
+    this->C = _C;
+    Rec.setSize(sf::Vector2f(size, size));
+    Rec.setFillColor(C);
+    Rec.setPosition(ri, ci);
+}
+
+void Cell::Draw(RenderWindow& window)
+{
+    window.draw(Rec);
+}
+
+void Cell::Highlight(RenderWindow& window,Color _C)
+{
+    Rec.setFillColor(sf::Color::Cyan);
+}
+
+void Cell::UnHighlight(RenderWindow& window)
+{
+    Rec.setFillColor(C);
+}
--- THE LUDO GAME/cell.h
+++ THE LUDO GAME/cell.h
@@ -1,5 +1,18 @@
 #pragma once
-class cell
+#include<SFML/Graphics.hpp>
+using namespace sf;
+
+class Cell
 {
-};
+private:
+	Color C;
+	int ri, ci;
+	int size;
+	RectangleShape Rec;
+public:
+	Cell(int _ri, int _ci,int _s,Color _C);
+	void Draw(RenderWindow& window);
+	void Highlight(RenderWindow& window, Color _C);
+	void UnHighlight(RenderWindow& window);
+}
 
