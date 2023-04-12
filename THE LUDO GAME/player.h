9c4354ab6bbc18c07b34feb6f14d8aeaeace780a
AmnaAmir6 <amnaamir605@gmail.com>
2023-04-12 16:48:49 +0500

    player class header and cpp

--- THE LUDO GAME/player.h
+++ THE LUDO GAME/player.h
@@ -1,8 +1,24 @@
 #pragma once
+#include<fstream>
+#include<SFML/Graphics.hpp>
+#include<string>
+using namespace sf;
+using namespace std;
+
  class player
 {
-
- 
- 
+private:
+	Color C;
+	string Name;
+	bool IsWin;
+	bool HasKilled;
+public:
+	player(string _name, Color, bool _isWin = false, bool _isKill = false);
+	Color getColor();
+	string getName();
+	bool isWin();
+	bool hasKilled();
+	void setIsWin(bool _win);
+	void setHasKilled(bool _kill);
 };
 
